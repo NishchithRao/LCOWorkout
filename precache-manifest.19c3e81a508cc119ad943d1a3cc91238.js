f0b6f2b6ecb0020d6e8cef1ce7e781db0fce5098
NishchithRao <devmonk4@gmail.com>
2020-04-16 22:32:49 +0530

    Updates

---
+++ precache-manifest.19c3e81a508cc119ad943d1a3cc91238.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057b92c32a5db9169e54e08e04e868b2",
+    "url": "/LCOWorkout/index.html"
+  },
+  {
+    "revision": "0726712462ab1e08bdb8",
+    "url": "/LCOWorkout/static/css/2.55146207.chunk.css"
+  },
+  {
+    "revision": "d056e8ed22fb752d6764",
+    "url": "/LCOWorkout/static/css/main.d317e662.chunk.css"
+  },
+  {
+    "revision": "0726712462ab1e08bdb8",
+    "url": "/LCOWorkout/static/js/2.03a0a4f4.chunk.js"
+  },
+  {
+    "revision": "e420ff54aaee9ec65dfee3e735742ec2",
+    "url": "/LCOWorkout/static/js/2.03a0a4f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d056e8ed22fb752d6764",
+    "url": "/LCOWorkout/static/js/main.5b904d83.chunk.js"
+  },
+  {
+    "revision": "2eb655f47b8d339602c4",
+    "url": "/LCOWorkout/static/js/runtime-main.0239b763.js"
+  },
+  {
+    "revision": "2b76583c8399d781cd83c505193bdd1b",
+    "url": "/LCOWorkout/static/media/1.2b76583c.mp3"
+  },
+  {
+    "revision": "a2bb97edc6179f81eb0a2afeccdcc562",
+    "url": "/LCOWorkout/static/media/14.a2bb97ed.mp3"
+  },
+  {
+    "revision": "4d743ca53d31e080e49d04fc3cbf0a82",
+    "url": "/LCOWorkout/static/media/18.4d743ca5.mp3"
+  },
+  {
+    "revision": "78d7e89566ad99662f7255940c5f3ccb",
+    "url": "/LCOWorkout/static/media/34.78d7e895.mp3"
+  },
+  {
+    "revision": "62947d5d1445b62a842849c3fb1378e3",
+    "url": "/LCOWorkout/static/media/50.62947d5d.mp3"
+  },
+  {
+    "revision": "b139d57eccbf6883a2019dea87411ced",
+    "url": "/LCOWorkout/static/media/profile.b139d57e.jpg"
+  }
+]);
